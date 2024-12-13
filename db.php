bad99ddf91baba0ae207afea70559e9d13920e8b
dhanush2927 <64139578+dhanush2927@users.noreply.github.com>
2024-12-13 17:08:40 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,6 @@
 <?php
-    $con = mysqli_connect("127.0.0.1","root","","communityconnect");
+    //$con = mysqli_connect("127.0.0.1","root","","communityconnect");
+$con = mysqli_connect("x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","r9zmvmyqqxt7lf2i","n6bu0312rntsdwwl","ga3l8rjztfd0s5zu");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
